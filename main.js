acc668002703807a56c547dc278e887627a5a3d2
Gojo9227 <vc874420@gmail.com>
2024-12-14 12:23:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+function copyText() {
+  const text = document.getElementById("minecraft-text").textContent;
+  navigator.clipboard.writeText(text).then(() => {
+  }).catch(err => {
+    console.error("Error copying text: ", err);
+  });
+}
+ 
